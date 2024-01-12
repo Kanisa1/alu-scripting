6237b0fc791f0e651580e431cf90e8c8df771678
Kanisa1 <k.thiak@alustudent.com>
2024-01-12 09:20:15 -0800

    8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
-str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
-str.split(/\w+:(.*), to:(.*), flags:(.*)/)
-puts str
+# script should output: [SENDER],[RECEIVER],[FLAGS]
 
+puts ARGV[0].scan(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*?)\]/).join(",")
